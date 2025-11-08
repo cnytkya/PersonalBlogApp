856057aa41a742bafbbe5c728733d08a441c6f2a
kayac <kayac@CUNEYT>
2025-11-08 11:57:49 +0300

    Add JWT settings and authentication DTOs
    Updated `appsettings.json` to include JWT configuration with `Issuer`, `Audience`, and `Key`. Added `AuthResponseDto`, `LoginDto`, and `RegisterDto` classes for handling authentication requests, with appropriate validation attributes for required fields.

---
+++ BlogApp.ApplicationLayer/DTOs/Auth/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApp.ApplicationLayer.DTOs.Auth
+{
+    public class LoginDto
+    {
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
